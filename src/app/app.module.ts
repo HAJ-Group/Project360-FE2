c70622ba5ca5dbbe1df68812c8b26e9b4e09755d
hamza alawi <hamza_hm@hotmail.be>
2020-06-04 21:25:53 +0100

    Fusion of login and subscribe with home component ... !!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
 import {FormsModule} from '@angular/forms';
-import { SubscribeComponent } from './subscribe/subscribe.component';
 import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
@@ -17,8 +15,6 @@ import {HttpClientModule} from '@angular/common/http';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    LoginComponent,
-    SubscribeComponent
   ],
   imports: [
     BrowserModule,
