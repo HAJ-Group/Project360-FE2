a0a9b3a16beddbd71f226e0c2c1d994f122dc673
jaouad <jaouadaitassou@gmail.com>
2020-06-12 00:06:06 +0100

    Adding logged username and his token + methods for checking if both username and token are logged in

--- src/app/service/data/user-data.service.ts
+++ src/app/service/data/user-data.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 const SERVER = 'http://localhost:8000/api/';
+const AUTHENTICATED_USER = 'AuthenticatedUser';
+const TOKEN = 'token';
 
 export class LoginAccount {
 
@@ -45,7 +48,16 @@ export class UserDataService {
   ) { }
 
   postLogin(account) {
-    return this.http.post<LoginAccount>(SERVER + 'login', account);
+    return this.http.post<LoginAccount>(SERVER + 'login', account).pipe(
+      map(
+        data =>
+            {
+              sessionStorage.setItem(TOKEN, 'Bearer ' + data);
+              sessionStorage.setItem(AUTHENTICATED_USER, account.username);
+              return data;
+            }
+      )
+    );
   }
 
   postSubscribe(account) {
@@ -60,6 +72,23 @@ export class UserDataService {
     return this.http.get(SERVER + 'confirm/' + username + '/' + code);
   }
 
+
+  getAuthenticatedUser() {
+    return sessionStorage.getItem(AUTHENTICATED_USER);
+  }
+
+
+  getAuthenticatedToken() {
+    if (this.getAuthenticatedUser()) {
+      return sessionStorage.getItem(TOKEN);
+    }
+  }
+
+
+  isUserloggedIn() {
+    const user = sessionStorage.getItem(AUTHENTICATED_USER);
+    return !(user === null);
+  }
 }
 
 
