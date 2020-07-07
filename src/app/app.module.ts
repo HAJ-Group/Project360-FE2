286aba337d6f79ffa06b66c094736d3992eb7fa4
jaouad <jaouadaitassou@gmail.com>
2020-07-07 12:03:28 +0100

    AnnounceModel instead of AnnonceModel => 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { ServicesService } from './footer/services.service';
 import {HTTP_INTERCEPTORS, HttpClientModule, HttpHeaders} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AnnonceDataService} from './service/data/annonce-data.service';
-import {AnnonceModel} from './model.ts/annonce-model';
+import {AnnounceModel} from './model.ts/announce-model';
 import {AnnoncerDataService} from './service/data/annoncer-data.service';
 import {OktaAuthModule} from '@okta/okta-angular';
 import {HttpAuthenticationInterceptorService} from './service/http/http-authentication-interceptor.service';
