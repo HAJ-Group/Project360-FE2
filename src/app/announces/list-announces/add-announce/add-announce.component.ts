cf245330aa2bfebd0577836430d0f1360e23aceb
jaouad <jaouadaitassou@gmail.com>
2020-07-10 14:37:25 +0100

    Enabling changing data between AnnounceImagesComponent and AddAnnounceComponent using ViewChild ...

--- src/app/announces/list-announces/add-announce/add-announce.component.ts
+++ src/app/announces/list-announces/add-announce/add-announce.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import {CITIES} from '../../../app.constants';
 import {AnnounceModel} from '../../../model.ts/announce-model';
 import {AnnonceDataService} from '../../../service/data/annonce-data.service';
+import {AnnounceImagesComponent} from './announce-images/announce-images.component';
 
 @Component({
   selector: 'app-add-announce',
   templateUrl: './add-announce.component.html',
   styleUrls: ['./add-announce.component.css']
 })
-export class AddAnnounceComponent implements OnInit {
+export class AddAnnounceComponent implements OnInit, AfterViewInit {
 
   cities: string[] = CITIES;
   announce: AnnounceModel;
+  selectedFiles: [] = [];
+
+  @ViewChild(AnnounceImagesComponent) child;
 
   constructor(
     private announceDataService: AnnonceDataService
@@ -22,14 +26,18 @@ export class AddAnnounceComponent implements OnInit {
       new AnnounceModel(1, '', '', '', 0, '', '', '', '', '', false, 0);
   }
 
+  ngAfterViewInit(){
+    this.selectedFiles = this.child.selectedFiles;
+  }
+
   addAnnounce() {
+
     this.initErrors();
-    this.announceDataService.createAnnounce('jaouad', this.announce).subscribe(
+    this.announceDataService.createAnnounce('jaouad', this.announce, this.selectedFiles).subscribe(
       success => {
         console.log(success);
       },
       error => {
-
         console.log(error);
         if (typeof error.error === 'object') {
           // tslint:disable-next-line:forin
@@ -39,6 +47,16 @@ export class AddAnnounceComponent implements OnInit {
         }
       }
     );
+
+    /*
+    this.announceDataService.storeImage(this.selectedFiles).subscribe(
+      success => {
+        console.log(success);
+      },
+      error => {
+        console.log(error);
+      }
+    );*/
   }
 
 
