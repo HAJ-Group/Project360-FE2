1a14f05f9513aedc73c3726091ab3324e33b8a49
jaouad <jaouadaitassou@gmail.com>
2020-07-12 14:20:23 +0100

    Attach images from the server with the parent announce => FRONT-END

--- src/app/announces/list-announces/list-announces.component.ts
+++ src/app/announces/list-announces/list-announces.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AnnonceDataService} from '../../service/data/annonce-data.service';
+import {SERVER_IMAGES_PATH} from '../../app.constants';
 
 @Component({
   selector: 'app-list-announces',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListAnnouncesComponent implements OnInit {
 
-  constructor() { }
+  myAnnounces: any[];
+  // 'jaouad' should be the current username
+  imagesDirectoryPath: any = SERVER_IMAGES_PATH + '/jaouad/';
+  constructor(
+    private announceDataService: AnnonceDataService
+  ) { }
 
   ngOnInit(): void {
+      this.announceDataService.getSpecificAnnounces('jaouad').subscribe(
+        success => {
+          console.log(success);
+          this.myAnnounces = success;
+        },
+        error => {
+          console.log(error);
+        }
+      );
   }
 
 }
