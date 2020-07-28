3c43b17a96d9a88be0db3cc56962342469b99726
Ennouinou Alae <alaeddinne.ennouinou@gmail.com>
2020-07-28 17:21:57 +0100

    search position , add menu header to search component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,7 +23,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.getAnnonces();
-    this.getPremiumAnnonces();
+  //  this.getPremiumAnnonces();
   }
 
   getAnnonces() {
