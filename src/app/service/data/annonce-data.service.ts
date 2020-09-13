c715fa2a2ae62b40c63a29fd6dbf2e756ff09689
hamza alawi <hamza_hm@hotmail.be>
2020-09-13 13:31:25 +0100

    Commit good

--- src/app/service/data/annonce-data.service.ts
+++ src/app/service/data/annonce-data.service.ts
@@ -5,7 +5,7 @@ import {AnnounceModel} from '../../model.ts/announce-model';
 import {SERVER_USERS} from '../../app.constants';
 
 
-const SERVER = 'http://aruesay.site:8000/api/annonces';
+const SERVER = 'http://localhost:8000/api/annonces';
 
 @Injectable()
 export class AnnonceDataService{
